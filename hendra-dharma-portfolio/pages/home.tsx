bc36f3cc39d6fc52166cd9263e0fff56310cfc3e
hendrawidjaya <hendrawidjaya90@gmail.com>
2023-01-06 11:04:33 +0100

    Import styles from Home.module.css to home.tsx

---
+++ hendra-dharma-portfolio/pages/home.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+    return (
+        <div className={styles.home_main_container}>
+            <h1>Hello from Home page!</h1>
+        </div>
+    );
+};
